4384fcb3e3f02d26e6f7df91fa38206126cba07a
Ayşıl Simge Karacan <github@aysilsimge.dev>
2024-01-28 16:31:45 +0300

    build(project): create project

---
+++ Plant Monitoring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plant Monitoring
+//
+//  Created by Ayşıl Simge Karacan on 28.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
