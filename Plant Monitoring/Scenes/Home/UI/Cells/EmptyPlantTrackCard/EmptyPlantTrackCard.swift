0fa09223ae556625077682e0d5406d016ea9f1fa
Ayşıl Simge Karacan <github@aysilsimge.dev>
2024-03-05 17:24:20 +0300

    [6] - feat(Home): continue to implement EmptyPlantTrackCard

---
+++ Plant Monitoring/Scenes/Home/UI/Cells/EmptyPlantTrackCard/EmptyPlantTrackCard.swift
@@ -0,0 +1,12 @@
+//
+//  EmptyPlantTrackCard.swift
+//  Plant Monitoring
+//
+//  Created by Ayşıl Simge Karacan on 5.03.2024.
+//
+
+import UIKit
+
+public final class EmptyPlantTrackCard: UITableViewCell {
+
+}
