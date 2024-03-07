0f762d779f51326090d08a788fa09c823b093481
Ayşıl Simge Karacan <github@aysilsimge.dev>
2024-03-07 09:06:10 +0300

    [11] - feat(TabBar): change tab bar style (#7)

--- Plant Monitoring/Common/Colors/Colors.swift
+++ Plant Monitoring/Common/Colors/Colors.swift
@@ -10,4 +10,6 @@ import UIKit
 public struct Colors {
   public static let plantCrystalGreen = UIColor(named: "plant-crystal-green")!
   public static let plantLightPastelPurple = UIColor(named: "plant-light-pastel-purple")!
+  public static let plantDeepMossGreen = UIColor(named: "plant-deep-moss-green")!
+  public static let plantMiddleGreen = UIColor(named: "plant-middle-green")!
 }
